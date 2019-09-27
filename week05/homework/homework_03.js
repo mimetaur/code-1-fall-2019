2cfa135c285e963e4b31613c3870045913e6c7b1
Nathan Koch <nathanrkoch@gmail.com>
2019-09-27 14:13:29 -0400

    homework 3

---
+++ week05/homework/homework_03.js
@@ -0,0 +1,18 @@
+function setup() {
+    createCanvas(300, 300);
+}
+
+function draw() {
+    background(180);
+    rectMode(CENTER);
+    fill("#EE3366");
+    // a coordinate-related function needs to be used here
+    angleMode(DEGREES);
+    // a coordinate-related function needs to be used here
+    // a coordinate-related function needs to be used here
+    rect(0, 0, 50, 50);
+    rect(100, 0, 50, 50);
+    rect(200, 0, 50, 50);
+    rect(300, 0, 50, 50);
+    // a coordinate-related function needs to be used here
+}
