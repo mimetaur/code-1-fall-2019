8d529fb2b0ad31db9e0ea03cf296115b483210dd
Nathan Koch <nathanrkoch@gmail.com>
2019-10-09 09:24:08 -0400

    homework 4 for week 6

---
+++ week06/homework_04.js
@@ -0,0 +1,12 @@
+function setup() {
+    createCanvas(400, 400);
+
+    background(220);
+    noStroke();
+    fill(80);
+}
+
+function draw() {
+
+    drawSpiral(width / 2, height / 2); // write the function definition for this line
+}
